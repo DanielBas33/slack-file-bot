8c780fb3e72a0bb59f243909877859967909e400
Daniel Bas <danielbas@Daniels-MacBook-Pro.local>
2022-04-30 15:39:19 +0200

    Slack bot to upload files with Go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3415835443671-3434029806516-oYm8K4HJ6q9hsBwxrJOMHnuI")
+	os.Setenv("CHANNEL_ID", "C03CQNTA43W")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"example.txt"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s", file.Name, file.URL)
+	}
+}
